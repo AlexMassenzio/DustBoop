03dd11df4103654b586d58f3187737e451da6e88
AlexMassenzio <alexmassenzio@gmail.com>
2017-08-05 10:12:32 -0400

    Removed test command

--- message_handler.py
+++ message_handler.py
@@ -3,15 +3,7 @@ import asyncio
 
 @asyncio.coroutine
 def process_message(message, client):
-    if message.content.startswith('!test'):
-        counter = 0
-        tmp = yield from client.send_message(message.channel, 'Calculating messages...')
-        async for log in client.logs_from(message.channel, limit=100):
-            if log.author == message.author:
-                counter += 1
-
-        yield from client.edit_message(tmp, 'You have {} messages.'.format(counter))
-    elif message.content.startswith('!ggquiz'):
+    if message.content.startswith('!ggquiz'):
         yield from client.send_message(message.channel, 'http://kalavinka.co.uk/GUILTY/')
     elif message.content.startswith('!bbquiz'):
         yield from client.send_message(message.channel, 'http://kalavinka.co.uk/EMBER/')
