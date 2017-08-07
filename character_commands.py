8f50b7596e963f0dbe7df8c83c4076b79e569973
AlexMassenzio <alexmassenzio@gmail.com>
2017-08-07 18:15:06 -0400

    testing image lookup

--- character_commands.py
+++ character_commands.py
@@ -7,6 +7,7 @@ import json
 @asyncio.coroutine
 def character_preview(message, client):
 	yield from client.send_message(message.channel, "Looking up character")
+
 	with open('data/characters.json') as json_data:
 		if len(message.content.split()) < 2:
 			yield from client.send_message(message.channel, "Please inclue a character name")
@@ -14,12 +15,17 @@ def character_preview(message, client):
 
 		character_name = message.content.split()[1]
 		character_data = json.load(json_data)
+
 		for character in character_data['characters']:
 			if character_name == character['name']:
 				url = 'http://www.dustloop.com/wiki/api.php?action=query&format=json&prop=images&titles=' + character['title'] + '&imlimit=50'
+			
 			response = yield from aiohttp.request('get', url)
 			string = (yield from response.read()).decode('utf-8')
 			data = json.loads(string)
-			answer = data['query']['normalized']
-			yield from client.send_message(message.channel, answer)
+			page_id = str(data['query']['pages'].keys()[0])
+			image_data = data['query']['pages'][page_id]['images']
 
+			for image in image_data
+				if 'Portrait' in image['title']:
+					yield from client.send_message(message.channel, image['title'])
