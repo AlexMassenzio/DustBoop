4560f6b1bf7711c833cf57c0d0e3fa91af3c7cee
AlexMassenzio <alexmassenzio@gmail.com>
2017-08-07 17:23:49 -0400

    support for character name in preview

--- character_commands.py
+++ character_commands.py
@@ -7,7 +7,13 @@ import json
 @asyncio.coroutine
 def character_preview(message, client):
 	 with open('data/character.json') as json_data:
+	 	if message.split().length < 2:
+	 		yield from client.send_message(message.channel, "Please inclue a character name")
+	 		return
+
+	 	character_name = message.split()[1]
         character_data = json.load(json_data)
         for character in help_data['characters']:
-            #implement lookup here
-        yield from client.send_message(message.channel, "TBD")
+            if character_name == character['name']:
+        	yield from client.send_message(message.channel, character['title'])
+            	
