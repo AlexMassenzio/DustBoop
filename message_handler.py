3e7ee3ababaf6c0147674143aafce6060d62b833
Massenzio <amassenzio@wiley.com>
2017-08-07 11:06:24 -0400

    Small fixes and debug statements

--- message_handler.py
+++ message_handler.py
@@ -14,11 +14,14 @@ def process_message(message, client):
     elif message.content.startswith('!ggreplays'):
         yield from client.send_message(message.channel, 'http://keeponrock.in')
     elif message.content.startswith('!test'):
+        yield from client.send_message(message.channel, 'here we go...')
         url = 'http://www.dustloop.com/wiki/api.php?action=parse&format=json&page=BBCF%2FHibiki_Kohaku&prop=displaytitle'
         response = yield from aiohttp.request('get', url)
         string = (yield from response.read()).decode('utf-8')
         data = json.loads(string)
         yield from client.send_message(message.channel, data)
+        yield from client.send_message(message.channel, 'made it!')
     elif message.content.startswith('!restart'):
+        yield from client.send_message(message.channel, 'Restarting...')
         subprocess.call(['shutdown', '-r', 'now'])
         yield from client.logout()
