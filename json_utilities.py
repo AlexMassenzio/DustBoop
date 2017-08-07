93b1177f57076daadacba87398ac98cc54be1dc8
AlexMassenzio <alexmassenzio@gmail.com>
2017-08-07 18:59:39 -0400

    moved string prep to command side

--- json_utilities.py
+++ json_utilities.py
@@ -4,8 +4,6 @@ import json
 
 @asyncio.coroutine
 def json_from_url(url):
-	url = url.replace(' ', '+')
-	url = url.replace(':', '%3A')
 	print(url)
 	response = yield from aiohttp.request('get', url)
 	string = (yield from response.read()).decode('utf-8')
